7e923eb6b01b18ffdd597dae0810ca15ad844657
Gustavo da Rosa Bitencourt <45208910+GustavoBitencourt@users.noreply.github.com>
2019-05-21 17:47:33 -0300

    Modal Alterar Produto
    Fiz o modal da lista de produtos com o fomrulário direitinho, só falta ajeitar a conexão do formulário com o banco pra funcionar a alteração

--- logado/listaDeProdutos.php
+++ logado/listaDeProdutos.php
@@ -30,7 +30,7 @@
             <td><?php echo $produto['2'];?></td>
             <td><?php echo $produto['1'];?></td>
             <td><img src="ImgProdutos/<?php echo $produto['3'];?>" class="imgprodutos"></td>
-            <td><a href="alteraProduto.php/?cliente=<?php echo $produto['0'];?>"><button>Alterar Produto</button></td>
+            <td><a href="#/?cliente=<?php echo $produto['0'];?>" class="abremodal"><button>Alterar Produto</button></td>
         </tr>
 
 <?php
@@ -42,4 +42,44 @@
 <?php
 }
 ?>
+
+    <div id="modal-form" class="modal-container">
+        <div class="modal">
+            <button class="fechar">x</button>
+            <h3 class="subtitulo">Alterar Produto</h3>
+            <form action="alterarProduto.php" method="get">
+                <br><input type="text" name="nomeDoProduto" class="input" placeholder="Nome do Produto"><br><br>
+                <input type="text" class="input"  name="descricao" placeholder="Descrição do Produto"><br><br>
+                <input type="text" class="input"  name="valor" placeholder="Valor do Produto"><br><br>
+
+                <form action="executaUpload.php" method="POST"  enctype="multipart/form-data">
+                <input type="hidden" name="MAX_FILE_SIZE" value="200000">
+                Foto:<input type="file" name="imagem" placeholder="Imagem">
+
+                <!-- <input type="file" name="imagem"  name="imagem" placeholder="Imagem" class="enviar"> --><br><br>
+                <input type="submit" class="button" name="cadastrar" value="Cadastrar">
+            </form>
+        </form>
+        </div>
+    </div>
+
+<script>
+    function iniciaModal(modalID) {
+               const modal = document.getElementById(modalID);
+                if(modal) {
+                    modal.classList.add('mostrar');
+                    modal.addEventListener('click', (e) => {
+                        if(e.target.id == modalID || e.target.className == 'fechar') {
+                            modal.classList.remove('mostrar');
+                        }
+                    });
+                }
+            }
+        
+
+        const mod = document.querySelector('.abremodal');
+        mod.addEventListener('click', () => iniciaModal('modal-form'));
+
+</script>
+
 <a href="index.php">Voltar</a>
