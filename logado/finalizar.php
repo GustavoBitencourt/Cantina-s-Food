c230187ca04c44e4bebc79238996c2e5b4c40ff2
IgorgsTSI <45207382+IgorgsTSI@users.noreply.github.com>
2019-06-18 16:18:31 -0300

    Update finalizar.php

--- logado/finalizar.php
+++ logado/finalizar.php
@@ -1,15 +1,31 @@
+    
 <?php
 session_start();
 require_once "conecta.php";
 
-if(isset($_POST['finalizar'])){
-	$dataAtual= date('Y-m-d');
-	$sqlRealizarPedido = mysql_query("INSERT INTO pedido (valor_total, data) VALUES ('$total', '$dataAtual')");
 
+if(isset($_POST['finalizar'])) {
+	if (!empty($_POST['valor_total'])) {
+		$valorTotal=$_POST['valor_total'];
+		$dataAtual= date('Y-m-d');
+
+		$comando="INSERT INTO pedido (valor_total, data) VALUES ('$valorTotal', '$dataAtual')";
+		$enviar=mysqli_query($conn, $comando);
+		
+		
+
+
+
+
+
+
+	
+	
 	$Enviar = mysql_insert_id($sqlRealizarPedido);
-
 ?>			
 	<script>alert("Pedido Realizado com Sucesso")</script>;
 			
 			
 }
+
+
