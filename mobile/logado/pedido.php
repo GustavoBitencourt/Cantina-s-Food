542a23d91eb43260a92cc8c33e431036f8b8b480
Gustavo da Rosa Bitencourt <45208910+GustavoBitencourt@users.noreply.github.com>
2019-06-30 20:51:52 -0300

    Barra de pesquisa mobile funcionando

--- mobile/logado/pedido.php
+++ mobile/logado/pedido.php
@@ -12,8 +12,19 @@
         echo $_SESSION['mensagem'];
         unset($_SESSION['mensagem']);
     }
-?>
 
+
+
+
+
+
+
+require_once("conecta.php");
+
+    // $comando="SELECT * FROM clientes WHERE id = {$_SESSION['id']}";
+    // $enviar=mysqli_query($conn, $comando);
+    
+ ?>
 
 <!DOCTYPE html>
 <html>
@@ -27,45 +38,112 @@
     <link rel="icon"  href="../../IMG/logo.png">
     <link href="https://fonts.googleapis.com/css?family=Mina:400,700&display=swap" rel="stylesheet">
 </head>
+        
+<!-- <header>
+
+        <nav>
+            <div class="nav-name">
+                <a href="index.php"><div class="container">CantIF</div></a>
+            </div>
+
+            <div class="nav-menu">
+                <div class="container"><img onclick="abrir()" src="../../IMG/menu.png"></div>
+            </div>
+        </nav>
 
 
-<?php if(array_key_exists("removido", $_GET) && $_GET['removido']=='true') { ?>
-    <p class="alert-success">Produto removido com sucesso!</p>
-<?php } ?>
 
-    <table>
-        <tr>
-            <th>Produto</th>
-            <th>Valor</th>
-            <th>Imagem</th>
-            <th>Adicionar</th>
-        </tr>
+
+         <div id="menu" class="menu">
+        <div class="container">
+            <a id="avatar" href="#"> -->
+                
+<?php
+   if (mysqli_num_rows($enviar)>0) :
+        while($dado = mysqli_fetch_array($enviar)):
+     ?>
+                <img src="../imgPerfil/<?php echo $dado['imagem'];?>" class="logBlock">
+                <div class="textuser"><?php echo $dado['nome'];?></div>
+                <?php 
+                endwhile;
+                else: ?>
+                    <img src="../imgPerfil/user.png" class="logBlock">
+                    <?php 
+                endif;
+               ?>
+
+                    
+
+
+
+
+            <!-- </a><div class="linha"></div>
+            <a href="#" style="margin-top: 30px;"><img src="../IMG/config.png" id="icon">Pedidos</a>
+            <a href="#" style="margin-top: 10px;"><img src="../IMG/pedidos.png" id="icon">Configurações</a>
+            <a href="../index.php" style="margin-top: 10px;"><img src="../IMG/logout.png" id="icon">Logout</a> -->
+            
+        </div>
+    </div>
+
+        <div class="busca">
+            <form action="pesquisar.php" method="POST" class="pesquisar">
+                <input type="text" name="pesquisar" placeholder="Pesquisar Produto" class="inputnome">
+                <button name="SendPesq" type="submit" class="imgpesq"></button>
+            </form>
+        </div>
+    
+    <div class="produtos">
+        <div class="areaproduto" >
 
         <?php
     foreach ($resultado as $produto) :
     
 ?>
-        <tr>
-            <td><?php echo $produto['2'];?></td>
-            <td><?php echo $produto['1'];?></td>
-            <td><img src="../../logado/ImgProdutos/<?php echo $produto['3'];?>" class="imgprodutos"></td>
-            <td><button>Adicionar</button></td>
+
+            <article id="itens">
+            <div class="none"> <div class="nomeproduto"><?php echo $produto['2'];?></div>
+            <div class="preco">R$ <?php echo $produto['1'];?></div>
+            <div class="imagemproduto"><img src="../../logado/ImgProdutos/<?php echo $produto['3'];?>" class="imgprodutos"></div>
+            <div class="comprar"><button id="comp">Comprar</button></div></div></article>
             
-        </tr>
+        
 
 
 <?php
     endforeach;
 ?>
+<br> <br>
+<!-- <button><a href="index.php">Voltar</a></button> -->
+</div>
+</div>
+    
 
-    </table>
-
-    <div class="finalizar">
+   <!--  <div class="finalizar">
     <button href="index.php">Finalizar</button>   
-    </div>
+    </div> -->
             
 <?php
 }
 ?>
-<br>
-<button><a href="index.php">Voltar</a></button>
+
+</html>
+
+<script>
+    
+    function abrir() {
+
+    var el = document.getElementById('menu')
+
+    console.log(el.style.display)
+
+    if (el.style.display == 'none' || el.style.display == '') {
+        el.style.display = 'flex';
+    } else {
+        el.style.display = 'none';
+    }
+}
+
+if (window.innerWidth > 450) {
+    alert("Para visualizar, utilize um dispositivo mobile, ou abra em modo desenvolvedor (F12) e logo após ative o modo mobile Ctrl+Shift+M .");
+}
+</script>
